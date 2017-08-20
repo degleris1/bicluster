893ef691dd16d2e9f00dae2a4970c02ac6826c7e
Anthony Degleris <degleris1@gmail.com>
2017-08-20 00:01:59 -0700

    completed modules

--- algorithms.py
+++ algorithms.py
@@ -1,26 +1,32 @@
-# Algorithms
-# Anthony Degleris
-# Last update: August 17th, 2017
+'''
+Algorithms
+Anthony Degleris
+Last update: August 18th, 2017
 
-# This module contains the algorithms produced during our research project
-# (as well as other algorithms used as benchmarks). 
-# Each algorithm should accept a data matrix, half the height of the submatrix 
-# (half is used to support split submatrices), and whether or not the submatrix 
-# is split. Finally, an algorithm should output the the column indices
-# recovered.
-# An index of the algorithms is provided to make accessing multiple algorithms 
-# easier.
-# Note: many of these functions assume proper usage, and will not handle bad
-# input.
+This module contains the algorithms produced during our research project
+(as well as other algorithms used as benchmarks). 
+Each algorithm should accept a data matrix, half the height of the submatrix 
+(half is used to support split submatrices), and whether or not the submatrix 
+is split. Finally, an algorithm should output the the column indices
+recovered.
+An index of the algorithms is provided to make accessing multiple algorithms 
+easier.
+Note: many of these functions assume proper usage, and will not handle bad
+input.
+'''
 
 import numpy as np
+import scipy.sparse.linalg as linear
 import matplotlib.pyplot as plt
 
 ITERATIONS = 20
 
-# ACCEPTS the data to search, the number of indices to return, whether
-# to search for the largest or smallest, and if sign should be ignored.
-# RETURNS an ordered list of the largest/smallest k indices.
+
+'''
+ACCEPTS the data to search, the number of indices to return, whether
+to search for the largest or smallest, and if sign should be ignored.
+RETURNS an ordered list of the largest/smallest k indices.
+'''
 def best_indices(data, k, best='largest', mag_only=False):   
     dtype = [('value', float), ('index', int)]
     if(mag_only):
@@ -35,45 +41,73 @@ def best_indices(data, k, best='largest', mag_only=False):
     else:
         return sorted(list(idm['index'][:k]))
 
+
+'''
+ACCEPTS a matrix, half the height of the bicluster, the row estimates,
+and whether the bicluster is split.
+RETURNS an estimate of the columns.
+'''
+def rows_to_cols(M, k, rows1, rows2=None, split=False):
+    if (split):
+        csums = np.sum(M[rows1,:], axis=0) - np.sum(M[rows2,:], axis=0)
+        return best_indices(csums, 2*k)
+    else:
+        return best_indices(np.sum(M[rows1,:], axis=0), 2*k)
     
-# ACCEPTS a matrix, half the height of the submatrix, a row
-# estimate, and a column estimate.
-# EXECUTION iteratively uses the previous row/column estimate
-# to sum part of the matrix and create a new row/column estimate.
-# RETURNS the columns recovered.
+
+'''
+ACCEPTS a matrix, half the height of the bicluster, the column estimate,
+and whether the bicluster is split.
+RETURNS an estimate of the rows.
+'''
+def cols_to_rows(M, k, cols, split=False):
+    if (split):
+        rsums = np.sum(M[:,cols], axis=1)
+        return (best_indices(rsums, k, 'largest'), best_indices(rsums, k, 'smallest'))
+    else:
+        return best_indices(np.sum(M[:,cols], axis=1), 2*k)
+
+
+
+'''
+ACCEPTS a matrix, half the height of the submatrix, a row
+estimate, and a column estimate.
+EXECUTION iteratively uses the previous row/column estimate
+to sum part of the matrix and create a new row/column estimate.
+RETURNS the columns recovered.
+'''
 def recovery_full(M, k, rows=None, cols=None):
     if(rows == None):
-        rows = best_indices(np.sum(M[:,cols], axis=1), 2*k)
+        rows = cols_to_rows(M, k, cols)
     for i in range(0, ITERATIONS):
-        cols = best_indices(np.sum(M[rows,:], axis=0), 2*k)
-        rows = best_indices(np.sum(M[:,cols], axis=1), 2*k)
+        cols = rows_to_cols(M, k, rows)
+        rows = cols_to_rows(M, k, cols)
     return cols
 
 
-# ACCEPTS a matrix, half the height, each cluster's row estimate,
-# and a column estimate.
-# EXECUTION iteratively uses the previous row/column estimate
-# to sum part of the matrix and create a new row/column estimate.
-# RETURNS the columns recovered.
+'''
+ACCEPTS a matrix, half the height, each cluster's row estimate,
+and a column estimate.
+EXECUTION iteratively uses the previous row/column estimate
+to sum part of the matrix and create a new row/column estimate.
+RETURNS the columns recovered.
+'''
 def recovery_split(M, k, rows1=None, rows2=None, cols=None):
     if(rows1 == None or rows2 == None):
-        rsums = np.sum(M[:,cols], axis=1)
-        rows1 = best_indices(rsums, k, 'largest')
-        rows2 = best_indices(rsums, k, 'smallest')
+        rows1, rows2 = cols_to_rows(M, k, cols, True)
     for i in range(0, ITERATIONS):
-        csums = np.sum(M[rows1,:], axis=0) - np.sum(M[rows2,:], axis=0)
-        cols = best_indices(csums, 2*k)
-        rsums = np.sum(M[:,cols], axis=1)
-        rows1 = best_indices(rsums, k, 'largest')
-        rows2 = best_indices(rsums, k, 'smallest')
+        cols = rows_to_cols(M, k, rows1, rows2, True)
+        rows1, rows2 = cols_to_rows(M, k, cols, True)
     return cols
         
 
-# ACCEPTS a matrix, half the height of the submatrix, and whether to
-# display graphics.
-# EXECUTION sums the rows of the matrix, choosing the best indices
-# by their rows.
-# RETURNS the column indices recovered.
+'''
+ACCEPTS a matrix, half the height of the bicluster, whether the
+bicluster is split, and whether to display graphics.
+EXECUTION sums the rows of the matrix, choosing the best indices
+by their rows.
+RETURNS the column indices recovered.
+'''
 def counting(M, k, split=False, graphics=False):
     if(split):
         rsums = np.sum(M, axis=1)
@@ -83,7 +117,91 @@ def counting(M, k, split=False, graphics=False):
     else:
         return recovery_full(M, k, best_indices(np.sum(M, axis=1), 2*k), None)
     
+
+'''
+ACCEPTS a matrix, half the height of the bicluster, whether the 
+bicluster is split, and whether to display graphics.
+EXECUTION uses singular value decomposition to estimate the columns
+of the bicluster.
+RETURNS the column indices recovered.
+'''
+def spectral(M, k, split=False, graphics=False):
+    u, s, vt = linear.svds(M, 1)
+    if (split):
+        return recovery_split(M, k, None, None, best_indices(vt[0,:], 2*k, mag_only=True))
+    else:
+        return recovery_full(M, k, None, best_indices(vt[0,:], 2*k, mag_only=True))
+    
+
+'''
+ACCEPTS a matrix, half the height of the bicluster, and whether to
+display graphics.
+See parent function for execution details.
+'''
+def recursive_spectral_helper(M, k, graphics):
+    u, s, vt = linear.svds(M, 1)
+    n = len(M)
+    if (n <= 5*k):
+        return best_indices(vt[0,:], 2*k, mag_only=True)
+    else:
+        cols = best_indices(vt[0,:], int(9*n/10), mag_only=True)
+        M = M[:, cols]
+        u, s, vt = linear.svds(M, 1)
+        rows = best_indices(u[:,0], int(9*n/10), mag_only=True)
+        return [cols[i] for i in recursive_spectral_helper(M[rows,:], k, graphics)]
     
     
+'''
+ACCEPTS a matrix, half the height of the bicluster, whether the bicluster
+is split, and whether to display graphics.
+EXECUTION recursively removes columns and rows using SVD to estimate
+the bicluster.
+RETURNS an estimate of the columns.
+'''
+def recursive_spectral(M, k, split=False, graphics=False):
+    M_copy = M.copy()
+    cols = recursive_spectral_helper(M_copy, k, graphics)
+    if (split):
+        return recovery_split(M, k, None, None, cols)
+    else:
+        return recovery_full(M, k, None, cols)
     
-run_alg = {'counting':counting}
+
+'''
+ACCEPTS a matrix, half the height of the bicluster, whether the
+bicluster is split, and whether to display graphics.
+See parent function for execution details.
+'''
+def recursive_counting_helper(M, k, split, graphics):
+    n = len(M)
+    rsums = np.sum(M, axis=1)
+    if (n <= 5*k):
+        return counting(M, k, split, graphics)
+    if (split):
+        rows1 = best_indices(rsums, int(n/2)-1, 'largest')
+        rows2 = best_indices(rsums, int(n/2)-1, 'smallest')
+        rows = sorted(rows1 + rows2)
+        cols = rows_to_cols(M, len(rows), rows1, rows2, split)
+    else:
+        rows = best_indices(rsums, n-2, 'largest')
+        cols = rows_to_cols(M, n-2, rows, None, split)
+    return [cols[i] for i in recursive_counting_helper(M[rows,:][:,cols], k, split, graphics)]
+    
+
+'''
+ACCEPTS a matrix, half the height of the bicluster, whether the
+bicluster is split, and whether to display graphics.
+EXECUTION recursively removes rows and columns through enumeration
+to estimate the bicluster.
+RETURNS the column indices recovered.
+'''
+def recursive_counting(M, k, split=False, graphics=False):
+    M_copy = M.copy()
+    cols = recursive_counting_helper(M_copy, k, split, graphics)
+    if (split):
+        return recovery_split(M, k, None, None, cols)
+    else:
+        return recovery_full(M, k, None, cols)
+    
+
+run_alg = {'cnt':counting, 'spc':spectral, 'rec_spc':recursive_spectral, 'rec_cnt':recursive_counting}
--- test_interface.py
+++ test_interface.py
@@ -1,31 +1,34 @@
-# Testing Interface
-# Anthony Degleris
-# Last update: August 17th, 2017
+'''
+Testing Interface
+Anthony Degleris
+Last update: August 19th, 2017
 
-# This module contains the the necessary functions to generate large amounts of
-# matrix data and test multiple algorithms at once.
+This module contains the the necessary functions to generate large amounts of
+matrix data and test multiple algorithms at once.
+'''
 
 import numpy as np
 import time
 import data_gen as gen
+import algorithms as alg
 
 
-
-# ACCEPTS the bicluster height, the number of trials for each matrix size, the number
-# of matrix sizes, the starting matrix size, the ending matrix size, and the error
-# rate in the cluster.
-# EXECUTION generates [trials] matrices at every resolution, spaced evenly
-# between nstart and nend. Each matrix is generated twice: once with a full cluster
-# and once with a split cluster. The distributions used are:
-#     noise: choices (-1, 1), cdf (0.5, 1)
-#     cluster1: choices (-1, 1), cdf (e, 1)
-#     cluster2: choices (-1, 1), cdf (1-e, 1)
-# RETURNS a list of the matrix sizes and a dictionary containing the matrices.
-def load_matrix_batch(k=10, trials=10, resolution=20, nstart=0.5, nend=1, e=0):
+'''
+ACCEPTS half the bicluster height, the number of trials for each matrix size, the number
+of matrix sizes, the starting matrix size, the ending matrix size, and the error
+rate in the cluster.
+EXECUTION generates [trials] matrices at every noise resolution, spaced evenly
+between nstart and nend. Each matrix is generated twice: once with a full cluster
+and once with a split cluster. The distributions used are:
+    noise: choices (-1, 1), cdf (0.5, 1)
+    cluster1: choices (-1, 1), cdf (e, 1)
+    cluster2: choices (-1, 1), cdf (1-e, 1)
+RETURNS a list of the matrix sizes and a dictionary containing the matrices.
+'''
+def load_matrix_nbatch(k=10, trials=10, resolution=20, nstart=0.5, nend=1, e=0):
     choices = (-1, 1)
     cdf = {'noise':(0.5, 1), 'cluster1':(e, 1), 'cluster2':(1-e, 1)}
     
-        
     nstart = int((nstart)*((2*k)**2))
     nend = int((nend)*(2*k)**2)   
     nlist = [int(i) for i in np.linspace(nstart, nend, resolution)]
@@ -43,3 +46,63 @@ def load_matrix_batch(k=10, trials=10, resolution=20, nstart=0.5, nend=1, e=0):
     print(' Done in ' + str((time.clock() - time_start)/60) + ' minutes.')
     return nlist, data
             
+
+'''
+ACCEPTS half the bicluster height, the matrix size, the number of points to sample,
+the number of trials, the starting error, and the ending error.
+EXECUTION generates [trials] matrices at every error resolution, spaced evenly
+between estart and eend. Each matrix is generated twice: once with a full cluster
+and once with a split cluster. The distributions used are:
+    noise: choices (-1, 1), cdf (0.5, 1)
+    cluster1: choices (-1, 1), cdf (e, 1)
+    cluster2: choices (-1, 1), cdf (1-e, 1)
+RETURNS a list of the errors and a dictionary containing the matrices.
+'''
+def load_matrix_ebatch(k=10, n=200, resolution=20, trials=10, estart=0, eend=0.1):
+    choices = (-1, 1)
+    cdf_noise = (0.5,1)
+    
+    elist = [i for i in np.linspace(estart, eend, resolution)]
+             
+    data = {}
+    time_start = time.clock()
+    print('Generating data')
+    for e in elist:
+        print('.', end='')
+        data[e] = {'full':[], 'split':[]}
+        for i in range(0, trials):
+            M = gen.create_matrix(n, n, choices, cdf_noise)
+            data[e]['full'].append(gen.add_cluster(M, 2*k, 2*k, choices, (e,1)))
+            data[e]['split'].append(gen.add_cluster(M.copy(), k, 2*k, choices, (1-e,1)))
+    print(' Done in ' + str((time.clock() - time_start)/60) + ' minutes.')
+    return elist, data
+
+
+'''
+ACCEPTS a function code (see algorithms module), half the size
+of the bicluster, the testing points (noise or error), the
+testing data, and whether or not the data is split.
+EXECUTION runs the algorithm on all the matrices in the data,
+recording its performance and runtime.
+RETURNS the performances and their runtimes.
+'''
+def test_algorithm(func_code, k, pts, data, split=False):
+    tests = {}
+    times = {}
+    
+    print('Testing ' + func_code, end='')
+    time_start = time.clock()
+    for pt in pts:
+        print('.', end='')
+        tests[pt] = {'split':[], 'full':[]}
+        times[pt] = {'split':[], 'full':[]}
+        
+        for bic_type in (('split', True), ('full', False)):
+            for matrix in data[pt][bic_type[0]]:
+                truth = set(range(0, 2*k))
+                start = time.clock()            
+                estimate = alg.run_alg[func_code](matrix, k, bic_type[1])
+                times[pt][bic_type[0]].append(time.clock() - start)
+                tests[pt][bic_type[0]].append(len(truth.intersection(estimate)) / (2*k))
+    print(' Done in ' + str((time.clock() - time_start)/60) + ' minutes.')
+    return tests, times
